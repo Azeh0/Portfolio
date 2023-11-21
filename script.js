aafe8ee6efb978716aed630c3721ac724cdf7846
Azeh0 <anthonyazevedo2005@gmail.com>
2023-11-20 19:24:17 -0500

    js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 function convertToAffiliate() {
-    const inputField = document.getElementById('amazonLinkInput');
-    const link = inputField.value.trim();
-    const affiliateTag = 'azevedo014-20'; // Replace with your affiliate tag
-  
-    if (link.includes('amazon')) {
-      let convertedLink = link;
-      if (!link.includes('tag=')) {
-        if (link.includes('?')) {
-          convertedLink += `&tag=${affiliateTag}`;
-        } else {
-          convertedLink += `?tag=${affiliateTag}`;
-        }
+  const inputField = document.getElementById('amazonLinkInput');
+  const link = inputField.value.trim();
+  const affiliateTag = 'azevedo014-20'; // Replace with your affiliate tag
+
+  if (link.includes('amazon')) {
+    let convertedLink = link;
+    if (!link.includes('tag=')) {
+      if (link.includes('?')) {
+        convertedLink += `&tag=${affiliateTag}`;
+      } else {
+        convertedLink += `?tag=${affiliateTag}`;
       }
-  
-      document.getElementById('convertedLink').innerHTML = `<a href="${convertedLink}" target="_blank">${convertedLink}</a>`;
-    } else {
-      document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link.';
     }
+
+    const iframe = document.createElement('iframe');
+    iframe.src = convertedLink;
+    iframe.style.width = '120px';
+    iframe.style.height = '240px';
+    iframe.style.border = 'none';
+
+    document.getElementById('convertedLink').innerHTML = '';
+    document.getElementById('convertedLink').appendChild(iframe);
+  } else {
+    document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link.';
   }
-  
-  
+}
