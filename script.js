9a85dcb53e38f1ad1e4046cb3fa0db2c01b8625c
Azeh0 <anthonyazevedo2005@gmail.com>
2023-11-21 01:23:38 -0500

    miod

--- script.js
+++ script.js
@@ -1,82 +1,48 @@
-function loadScript(url) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.src = url;
-    script.onload = resolve;
-    script.onerror = reject;
-    document.head.appendChild(script);
-  });
-}
+console.log('JavaScript code is loaded and executed');
 
-// Load Axios dynamically
-loadScript('https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js')
-  .then(() => {
-    // Axios is loaded, you can use it now
+window.convertToAffiliate = function() {
+  const inputField = document.getElementById('amazonLinkInput');
+  const link = inputField.value.trim();
+  const affiliateTag = 'azevedo014-20'; // Replace with your affiliate tag
 
-    async function getFullAmazonURL(shortenedURL) {
-      try {
-        const response = await axios.head(shortenedURL, { maxRedirects: 5 });
-        return response.request.res.responseUrl;
-      } catch (error) {
-        console.error('Error:', error.message);
-        return null;
-      }
-    }
+  const asin = extractASIN(link);
+  const linkId = extractLinkID(link);
 
-    function convertToAffiliate() {
-      const inputField = document.getElementById('amazonLinkInput');
-      const link = inputField.value.trim();
-      const affiliateTag = 'azevedo014-20'; // Replace with your affiliate tag
+  if (asin) {
+    const convertedLink = `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
 
-      const asin = extractASIN(link);
-      const linkId = extractLinkID(link);
+    const iframe = document.createElement('iframe');
+    iframe.src = convertedLink;
+    iframe.setAttribute('width', '120');
+    iframe.setAttribute('height', '240');
+    iframe.setAttribute('align-items', 'center');
+    iframe.setAttribute('scrolling', 'no'); // Add scrolling="no" attribute
+    iframe.style.overflow = 'hidden'; // Add overflow: hidden; CSS property
+    iframe.style.border = 'none';
 
-      if (asin) {
-        getFullAmazonURL(link)
-          .then((fullURL) => {
-            if (fullURL) {
-              const convertedLink = generateAmazonLink(asin, linkId, affiliateTag, fullURL);
-
-              const iframe = document.createElement('iframe');
-              iframe.src = convertedLink;
-              iframe.setAttribute('width', '120');
-              iframe.setAttribute('height', '240');
-              iframe.setAttribute('align-items', 'center');
-              iframe.setAttribute('scrolling', 'no');
-              iframe.style.overflow = 'hidden';
-              iframe.style.border = 'none';
-
-              const amazonLinksDiv = document.getElementById('amazonLinks');
-              amazonLinksDiv.innerHTML = '';
-              amazonLinksDiv.appendChild(iframe);
-            } else {
-              document.getElementById('convertedLink').innerHTML = 'Unable to retrieve the full URL.';
-            }
-          })
-          .catch((err) => console.error('Error:', err));
-      } else {
-        document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link or ASIN not found.';
-      }
-    }
-
-function extractASIN(link) {
-  if (link.includes('a.co/')) {
-    const parts = link.split('/');
-    const asinPart = parts.find(part => part.length === 7); // ASIN is assumed to be a 7-character string
-    return asinPart || '';
+    const amazonLinksDiv = document.getElementById('amazonLinks');
+    amazonLinksDiv.innerHTML = ''; // Clear previous content
+    amazonLinksDiv.appendChild(iframe);
   } else {
-    const asinMatch = link.match(/\/(?:dp|gp\/product|exec\/obidos|aw\/d)\/(B[0-9A-Z]{9}|\w{10})/);
-    return asinMatch ? asinMatch[1] : '';
+    document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link or ASIN not found.';
   }
 }
 
+function extractASIN(link) {
+  const asinMatch = link.match(/\/(?:dp|gp\/product|exec\/obidos|aw\/d)\/(B[0-9A-Z]{9}|\w{10})|a\.co\/(?:[^\/]+\/)?(\w{7})(\/|\?|#|$)/);
+  console.log('Link:', link);
+  console.log('Match:', asinMatch);
+  if (asinMatch) {
+    return asinMatch[1] || asinMatch[2];
+  } else {
+    return '';
+  }
+}
+
+ 
 function extractLinkID(link) {
   const linkIdMatch = link.match(/coliid=([\w\d]{10})/);
   return linkIdMatch ? linkIdMatch[1] : null;
-}
-
-function generateAmazonLink(asin, linkId, affiliateTag) {
-  return `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -85,9 +51,4 @@ document.addEventListener('DOMContentLoaded', function() {
   convertButton.addEventListener('click', function() {
     convertToAffiliate();
   });
-});
-
-})
-.catch((error) => {
-  console.error('Error loading Axios:', error);
 });
