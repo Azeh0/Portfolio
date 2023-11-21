d651e5e02e35a307e04287ba2cfedb0f5f1d6a8a
Azeh0 <anthonyazevedo2005@gmail.com>
2023-11-20 21:06:41 -0500

    Made it work for any azmazon link :)

--- script.js
+++ script.js
@@ -5,40 +5,36 @@ window.convertToAffiliate = function() {
   const link = inputField.value.trim();
   const affiliateTag = 'azevedo014-20'; // Replace with your affiliate tag
 
-  if (link.includes('amazon')) {
-    const asin = extractASIN(link);
-    const linkId = extractLinkID(link);
+  const asin = extractASIN(link);
+  const linkId = extractLinkID(link);
 
-    if (asin && linkId) {
-      const convertedLink = `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
+  if (asin) {
+    const convertedLink = `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
 
-      const iframe = document.createElement('iframe');
-      iframe.src = convertedLink;
-      iframe.setAttribute('width', '120');
-      iframe.setAttribute('height', '240');
-      iframe.setAttribute('scrolling', 'no'); // Add scrolling="no" attribute
-      iframe.style.overflow = 'hidden'; // Add overflow: hidden; CSS property
-      iframe.style.border = 'none';
+    const iframe = document.createElement('iframe');
+    iframe.src = convertedLink;
+    iframe.setAttribute('width', '120');
+    iframe.setAttribute('height', '240');
+    iframe.setAttribute('scrolling', 'no'); // Add scrolling="no" attribute
+    iframe.style.overflow = 'hidden'; // Add overflow: hidden; CSS property
+    iframe.style.border = 'none';
 
-      const amazonLinksDiv = document.getElementById('amazonLinks');
-      amazonLinksDiv.innerHTML = ''; // Clear previous content
-      amazonLinksDiv.appendChild(iframe);
-    } else {
-      document.getElementById('convertedLink').innerHTML = 'Invalid Amazon link format.';
-    }
+    const amazonLinksDiv = document.getElementById('amazonLinks');
+    amazonLinksDiv.innerHTML = ''; // Clear previous content
+    amazonLinksDiv.appendChild(iframe);
   } else {
-    document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link.';
+    document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link or ASIN not found.';
   }
 }
 
 function extractASIN(link) {
-  const match = link.match(/\/(dp|gp\/product)\/(\w{10})/);
-  return match ? match[2] : '';
+  const asinMatch = link.match(/\/(dp|gp\/product|exec\/obidos\/ASIN)\/(\w{10}|\w{9})(\/|\?|$)/);
+  return asinMatch ? asinMatch[2] : '';
 }
 
 function extractLinkID(link) {
-  const match = link.match(/coliid=([\w\d]{10})/);
-  return match ? match[1] : '';
+  const linkIdMatch = link.match(/coliid=([\w\d]{10})/);
+  return linkIdMatch ? linkIdMatch[1] : '';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
