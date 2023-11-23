45110b8c407ba87299518c299ac050ba5e285cd3
Azeh0 <anthonyazevedo2005@gmail.com>
2023-11-22 19:05:35 -0500

    Link converter not working

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
 console.log('JavaScript code is loaded and executed');
 
-function convertToAmazonCA(link) {
-  // Replace "a.co" with "amazon.ca"
-  const convertedLink = link.replace('a.co', 'amazon.ca');
-  return convertedLink;
+function extractASIN(link) {
+  const asinMatch = link.match(/\/(?:dp|gp\/product|exec\/obidos|aw\/d)\/(B[0-9A-Z]{9}|\w{10})|a\.co\/(?:[^\/]+\/)?(dp|gp\/product|exec\/obidos|aw\/d)\/(\w{10})/);
+  console.log('Link:', link);
+  console.log('Match:', asinMatch);
+  if (asinMatch) {
+    return asinMatch[1] || asinMatch[3];
+  } else {
+    return '';
+  }
+}
+
+function extractLinkID(link) {
+  const linkIdMatch = link.match(/coliid=([\w\d]{10})/);
+  return linkIdMatch ? linkIdMatch[1] : null;
 }
 
 window.convertToAffiliate = function() {
   const inputField = document.getElementById('amazonLinkInput');
   const link = inputField.value.trim();
   const affiliateTag = 'azevedo014-20'; // Replace with your affiliate tag
+  const amazonCALinkRegex = /https?:\/\/(www\.)?amazon\.ca/;
 
-   // Replace "a.co" with "amazon.ca" in the link
-   const convertedLink = convertToAmazonCA(link);
+if (link.match(amazonCALinkRegex)) {
+  const asin = extractASIN(link);
+  const linkId = extractLinkID(link);
+  const convertedAffiliateLink = `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
 
-   const asin = extractASIN(convertedLink);
-   const linkId = extractLinkID(convertedLink);
- 
-   if (asin) {
-     const convertedAffiliateLink = `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
- 
-     const iframe = document.createElement('iframe');
-     iframe.src = convertedAffiliateLink;
-     iframe.setAttribute('width', '120');
-     iframe.setAttribute('height', '240');
-     iframe.setAttribute('align-items', 'center');
-     iframe.setAttribute('scrolling', 'no'); // Add scrolling="no" attribute
-     iframe.style.overflow = 'hidden'; // Add overflow: hidden; CSS property
-     iframe.style.border = 'none';
- 
-     const amazonLinksDiv = document.getElementById('amazonLinks');
-     amazonLinksDiv.innerHTML = ''; // Clear previous content
-     amazonLinksDiv.appendChild(iframe);
-   } else {
-     document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link or ASIN not found.';
-   }
- }
- 
- function extractASIN(link) {
-   const asinMatch = link.match(/\/(?:dp|gp\/product|exec\/obidos|aw\/d)\/(B[0-9A-Z]{9}|\w{10})|a\.co\/(?:[^\/]+\/)?(dp|gp\/product|exec\/obidos|aw\/d)\/(\w{10})/);
-   console.log('Link:', link);
-   console.log('Match:', asinMatch);
-   if (asinMatch) {
-     return asinMatch[1] || asinMatch[3];
-   } else {
-     return '';
-   }
- }
- 
- function extractLinkID(link) {
-   const linkIdMatch = link.match(/coliid=([\w\d]{10})/);
-   return linkIdMatch ? linkIdMatch[1] : null;
- }
- 
- document.addEventListener('DOMContentLoaded', function() {
-   const convertButton = document.getElementById('convertButton');
- 
-   convertButton.addEventListener('click', function() {
-     convertToAffiliate();
-   });
- });
+      const iframe = document.createElement('iframe');
+      iframe.src = convertedAffiliateLink;
+      iframe.setAttribute('width', '120');
+      iframe.setAttribute('height', '240');
+      iframe.setAttribute('align-items', 'center');
+      iframe.setAttribute('scrolling', 'no');
+      iframe.style.overflow = 'hidden';
+      iframe.style.border = 'none';
+
+      const amazonLinksDiv = document.getElementById('amazonLinks');
+      amazonLinksDiv.innerHTML = ''; // Clear previous content
+      amazonLinksDiv.appendChild(iframe);
+  }
+
+  fetch('http://127.0.0.1:5000/convert', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ link: link })
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to convert link');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const amazonCALink = data.amazon_ca_link;
+    
+    const asin = extractASIN(link);
+    const linkId = extractLinkID(link);
+
+    if (amazonCALink && asin) {
+      const convertedAffiliateLink = `//rcm-na.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=${affiliateTag}&language=en_CA&o=15&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=${asin}&linkId=${linkId}`;
+
+      const iframe = document.createElement('iframe');
+      iframe.src = convertedAffiliateLink;
+      iframe.setAttribute('width', '120');
+      iframe.setAttribute('height', '240');
+      iframe.setAttribute('align-items', 'center');
+      iframe.setAttribute('scrolling', 'no');
+      iframe.style.overflow = 'hidden';
+      iframe.style.border = 'none';
+
+      const amazonLinksDiv = document.getElementById('amazonLinks');
+      amazonLinksDiv.innerHTML = ''; // Clear previous content
+      amazonLinksDiv.appendChild(iframe);
+    } else {
+      document.getElementById('convertedLink').innerHTML = 'Not a valid Amazon link or ASIN not found.';
+    }
+
+    console.log('Processed link:', amazonCALink);
+  })
+  .catch(error => console.error('Error:', error.message));
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  const convertButton = document.getElementById('convertButton');
+
+  convertButton.addEventListener('click', function() {
+    convertToAffiliate();
+  });
+});
--- test.py
+++ test.py
@@ -1,55 +1,98 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS # Import the CORS class
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import time
+import re
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/convert": {"origins": "*"}})
+
+def find_product_asin(a_co_link):
+ try:
+     response = requests.get(a_co_link)
+     response.raise_for_status()
+
+     soup = BeautifulSoup(response.content, 'html.parser')
+     amazon_ca_link = soup.find('a', href=lambda href: href and href.startswith('https://www.amazon.ca/dp/'))
+
+     if not amazon_ca_link:
+         print("Unable to find Amazon.ca link.")
+         return None
+
+     product_id = amazon_ca_link.get('href')
+     parsed_url = urlparse(product_id)
+     path_components = parsed_url.path.split('/')
+     asin_index = path_components.index('dp') + 1 if 'dp' in path_components else -1
+
+     if asin_index != -1 and len(path_components) > asin_index:
+         asin = path_components[asin_index]
+         return asin
+     else:
+         print("ASIN not found in the URL.")
+         return None
+
+ except requests.exceptions.HTTPError as e:
+     if e.response.status_code == 500:
+         print("URL being processed:", a_co_link) # Print the URL being processed
+         asin = re.search(r'/[dg]p/([^/]+)', a_co_link, flags=re.IGNORECASE)
+         if asin:
+             return asin.group(1)
+         else:
+             print("ASIN not found in the URL.")
+             return None
+     print(f"HTTPError: {str(e)}")
+     return None
+ except requests.exceptions.RequestException as e:
+     print(f"RequestException: {str(e)}")
+     return None
+ except Exception as e:
+     print(f"An unexpected error occurred: {str(e)}")
+     return None
 
 def convert_to_amazon_ca(a_co_link):
-   try:
-       # Get the ASIN from the a.co link
-       response = requests.get(a_co_link)
-       if response.status_code != 200:
-           print(f"Failed to fetch the page. Status code: {response.status_code}")
-           return None
-       
-       soup = BeautifulSoup(response.content, 'html.parser')
-       canonical_link = soup.find('link', {'rel': 'canonical'})
-       
-       if not canonical_link:
-           print("Unable to find canonical link.")
-           return None
-       
-       product_id = canonical_link.get('href')
-       
-       parsed_url = urlparse(product_id)
-       path_components = parsed_url.path.split('/')
-       asin_index = path_components.index('dp') + 1 if 'dp' in path_components else -1
-       
-       if asin_index != -1 and len(path_components) > asin_index:
-           asin = path_components[asin_index]
-       else:
-           print("ASIN not found in the URL.")
-           return None
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+    }
+    try:
+        # Check if the link is from Amazon.ca
+        if 'amazon.ca' in a_co_link:
+            # If it's an Amazon.ca link, return None or handle as needed
+            return None
+        response = requests.get(a_co_link, headers=headers)
+        response.raise_for_status()
+        time.sleep(2)
 
-       # Search for the ASIN on Amazon.ca
-       amazon_ca_link = f"https://www.amazon.ca/dp/{asin}"
-       return amazon_ca_link
-   
-   except Exception as e:
-       print(f"An error occurred: {str(e)}")
-       return None
+        asin = find_product_asin(a_co_link)
 
+        if not asin:
+            app.logger.error("ASIN not found in the URL.")
+            return None
+
+        amazon_ca_link = f"https://www.amazon.ca/dp/{asin}"
+        return amazon_ca_link
+
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 500:
+            return a_co_link
+        app.logger.error(f"HTTPError: {str(e)}")
+        return None
+    except requests.RequestException as e:
+        app.logger.error(f"Failed to fetch the page. Error: {str(e)}")
+        return None
+    except Exception as e:
+        app.logger.error(f"An error occurred: {str(e)}")
+        return None
 @app.route('/convert', methods=['POST'])
+@cross_origin()
 def handle_conversion():
-   data = request.get_json()
-   amazon_link = data.get('link')
+    data = request.get_json()
+    amazon_link = data.get('link')
 
-   amazon_ca_link = convert_to_amazon_ca(amazon_link)
+    amazon_ca_link = convert_to_amazon_ca(amazon_link)
 
-   return jsonify({'amazon_ca_link': amazon_ca_link})
+    return jsonify({'amazon_ca_link': amazon_ca_link})
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
